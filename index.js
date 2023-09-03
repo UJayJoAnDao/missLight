4cfb295b266b73932ece79a446fd0ce0765de3db
張侑傑 <plmoknijb1234510@gmail.com>
2023-09-03 15:56:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-let Btn1 = $('#btn1');
-let Btn2 = $('#btn2');
-let Btn3 = $('#btn3');
-let Btn4 = $('#btn4');
-let Btn5 = $('#btn5');
+let optionsBtn = $('button');
 let optionButton = $("#options");
 let circleButton = $("#myButton");
+optionButton.css("display","none");
 
-function send(data) {
+function send() {
   alert("已按下");
   $.ajax({
     type: "get",
     url: "https://script.google.com/macros/s/AKfycbyIOG5SsKFwUvupQJuL3h6EtQHzIJW0tMLsrPA5P-EGd4s7ddxMFPI1_kUp0XW6C-YG/exec",
     data: {
-      "name": data, /* 屬性名稱需與 Google Sheet 相同 */
+      "name": $(this).id, /* 屬性名稱需與 Google Sheet 相同 */
       "phone": "123", /* 屬性名稱需與 Google Sheet 相同 */
     },
     dataType: "JSON",
@@ -25,4 +22,11 @@ function send(data) {
   });
 };
 
-sendButton.click(send,"1");
+optionsBtn.click(send);
+optionButton.click(function(){
+  if($(this).css("display")=="none"){
+    $(this).css("display","flex");
+  }else{
+    $(this).css("display","none");
+  }
+);
