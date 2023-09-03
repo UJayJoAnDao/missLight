dc889c08b16c77137244fc992c847bfa543b6f21
張侑傑 <plmoknijb1234510@gmail.com>
2023-09-03 15:01:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let sendButton = $('#myButton');
 
 function send() {
+  alert("已按下");
   $.ajax({
     type: "get",
     url: "https://script.google.com/macros/s/AKfycbyIOG5SsKFwUvupQJuL3h6EtQHzIJW0tMLsrPA5P-EGd4s7ddxMFPI1_kUp0XW6C-YG/exec",
     data: {
-      "name": 123, /* 屬性名稱需與 Google Sheet 相同 */
-      "phone": 456, /* 屬性名稱需與 Google Sheet 相同 */
+      "name": "HI", /* 屬性名稱需與 Google Sheet 相同 */
+      "phone": "123", /* 屬性名稱需與 Google Sheet 相同 */
     },
     dataType: "JSON",
     success: function(response) {
